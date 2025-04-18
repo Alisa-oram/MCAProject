e959f3a130a775eb5f4a253eb685f0e11b62f57d
Subhamita24pani <panisubhamita@gmail.com>
2025-04-18 12:46:39 +0530

    login for coach and student

--- admin/add_coach.php
+++ admin/add_coach.php
@@ -1,4 +1,6 @@
 <?php
+require_once "../phpmailer/test.php";
+
 if(isset($_POST['upload'])){
    $name = $_POST['name'];
    $role = $_POST['role'];
@@ -9,6 +11,8 @@ if(isset($_POST['upload'])){
    require_once "admin_functions.php";
    $res = addCoach($name,$role,$club,$password,$email);
    if($res){
+      //send mail to coach
+      sendMail($email ,"Added to club","Dear sir,Ihope this mail finds you well.We are inform you that you are added to the club ".$club." you can logged in  through your email Id and Password :".$password);
       echo "<script>alert('Coach Added!'); window.location.href='add_coach.html';</script>";
    } else {
        echo " Error While Adding.";
