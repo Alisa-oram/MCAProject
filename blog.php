1d4d3e6ff8545c40dbbe9dece6816b6be2a497b8
Alisa-oram <alisaoram33@gmail.com>
2025-05-06 22:31:13 +0530

    create new page

---
+++ blog.php
@@ -0,0 +1,151 @@
+<head>
+    <style>
+        body {
+      background-color: #1f2937; /* dark background */
+      color: black;
+    }
+        .Pageblog{
+            position: relative;
+             width: 100%;
+              height: 750px;
+            }
+        .Blog{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: black;
+            font-size: 60px;
+            font-weight: bold;
+            text-shadow: 2px 2px 10px rgba(0,0,0,0.7);
+         }
+         
+    .card {
+      background-color: #1f2937;
+      border: none;
+    }
+    .card-title, .card-text, .card-date {
+      color:rgb(11, 11, 12);
+    }
+    .read-more {
+      color: #ef4444;
+      text-decoration: none;
+    }
+    .read-more:hover {
+      text-decoration: underline;
+    }
+    </style>
+</head>
+<?php 
+include_once "./fragments/navbar.php";
+?>
+<div class="Pageblog">
+  <img src="./assets/images/blog.jpg" alt="Football Club" style="width: 100%; height: 100%; object-fit: cover;">
+  <div class="Blog">Blogs Posts</div>
+</div>
+<div class="container py-5">
+  <h1 class="text-center mb-4">All Blogs</h1>
+  <div class="row g-4 justify-content-center">
+
+    <!-- Card 1 -->
+    <div class="col-md-4 col-lg-3 d-flex">
+      <div class="card d-flex flex-column w-100">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Neymar_against_Colombia_World_Cup_2014.jpg/800px-Neymar_against_Colombia_World_Cup_2014.jpg" class="card-img-top" alt="Card image">
+        <div class="card-body d-flex flex-column">
+          <small class="card-date mb-2">May 20, 2020</small>
+          <h5 class="card-title">Romolu to stay at Real Madrid?</h5>
+          <p class="card-text flex-grow-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Possimus deserunt saepe tempora dolorem.</p>
+          <a href="#" class="read-more mt-auto">Read more</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Card 2 -->
+    <div class="col-md-4 col-lg-3 d-flex">
+      <div class="card d-flex flex-column w-100">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Lionel_Messi_2014.jpg/800px-Lionel_Messi_2014.jpg" class="card-img-top" alt="Card image">
+        <div class="card-body d-flex flex-column">
+          <small class="card-date mb-2">May 20, 2020</small>
+          <h5 class="card-title">Romolu to stay at Real Madrid?</h5>
+          <p class="card-text flex-grow-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Possimus deserunt saepe tempora dolorem.</p>
+          <a href="#" class="read-more mt-auto">Read more</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Card 3 -->
+    <div class="col-md-4 col-lg-3 d-flex">
+      <div class="card d-flex flex-column w-100">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Neymar_against_Colombia_World_Cup_2014.jpg/800px-Neymar_against_Colombia_World_Cup_2014.jpg" class="card-img-top" alt="Card image">
+        <div class="card-body d-flex flex-column">
+          <small class="card-date mb-2">May 20, 2020</small>
+          <h5 class="card-title">Romolu to stay at Real Madrid?</h5>
+          <p class="card-text flex-grow-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Possimus deserunt saepe tempora dolorem.</p>
+          <a href="#" class="read-more mt-auto">Read more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 col-lg-3 d-flex">
+      <div class="card d-flex flex-column w-100">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Neymar_against_Colombia_World_Cup_2014.jpg/800px-Neymar_against_Colombia_World_Cup_2014.jpg" class="card-img-top" alt="Card image">
+        <div class="card-body d-flex flex-column">
+          <small class="card-date mb-2">May 20, 2020</small>
+          <h5 class="card-title">Romolu to stay at Real Madrid?</h5>
+          <p class="card-text flex-grow-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Possimus deserunt saepe tempora dolorem.</p>
+          <a href="#" class="read-more mt-auto">Read more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 col-lg-3 d-flex">
+      <div class="card d-flex flex-column w-100">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Neymar_against_Colombia_World_Cup_2014.jpg/800px-Neymar_against_Colombia_World_Cup_2014.jpg" class="card-img-top" alt="Card image">
+        <div class="card-body d-flex flex-column">
+          <small class="card-date mb-2">May 20, 2020</small>
+          <h5 class="card-title">Romolu to stay at Real Madrid?</h5>
+          <p class="card-text flex-grow-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Possimus deserunt saepe tempora dolorem.</p>
+          <a href="#" class="read-more mt-auto">Read more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 col-lg-3 d-flex">
+      <div class="card d-flex flex-column w-100">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Neymar_against_Colombia_World_Cup_2014.jpg/800px-Neymar_against_Colombia_World_Cup_2014.jpg" class="card-img-top" alt="Card image">
+        <div class="card-body d-flex flex-column">
+          <small class="card-date mb-2">May 20, 2020</small>
+          <h5 class="card-title">Romolu to stay at Real Madrid?</h5>
+          <p class="card-text flex-grow-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Possimus deserunt saepe tempora dolorem.</p>
+          <a href="#" class="read-more mt-auto">Read more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 col-lg-3 d-flex">
+      <div class="card d-flex flex-column w-100">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Neymar_against_Colombia_World_Cup_2014.jpg/800px-Neymar_against_Colombia_World_Cup_2014.jpg" class="card-img-top" alt="Card image">
+        <div class="card-body d-flex flex-column">
+          <small class="card-date mb-2">May 20, 2020</small>
+          <h5 class="card-title">Romolu to stay at Real Madrid?</h5>
+          <p class="card-text flex-grow-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Possimus deserunt saepe tempora dolorem.</p>
+          <a href="#" class="read-more mt-auto">Read more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 col-lg-3 d-flex">
+      <div class="card d-flex flex-column w-100">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Neymar_against_Colombia_World_Cup_2014.jpg/800px-Neymar_against_Colombia_World_Cup_2014.jpg" class="card-img-top" alt="Card image">
+        <div class="card-body d-flex flex-column">
+          <small class="card-date mb-2">May 20, 2020</small>
+          <h5 class="card-title">Romolu to stay at Real Madrid?</h5>
+          <p class="card-text flex-grow-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Possimus deserunt saepe tempora dolorem.</p>
+          <a href="#" class="read-more mt-auto">Read more</a>
+        </div>
+      </div>
+    </div>
+    
+
+  </div>
+</div>
+
+
+<?php
+include_once "./fragments/footer.php";
+?>
